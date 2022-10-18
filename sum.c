a301e9ee09fec913d424a52ae4355b61ab136f64
Tuhin Chandra <tuhinchandra2k04@gmail.com>
2022-10-18 10:33:34 +0530

    Sum is added

---
+++ sum.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main() {
+    int num1,num2;
+    printf("Enter the 1st number : ");
+    scanf("%d",&num1);
+    printf("Enter the 2nd number : ");
+    scanf("%d",&num2);
+    printf("The sum of %d and %d is %d",num1,num2,num1+num2);
+    return 0;
+}
