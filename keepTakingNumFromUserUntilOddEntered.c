c4a9b21a8111d4accc39b7b01063b6085341dc70
Tuhin Chandra <tuhinchandra2k04@gmail.com>
2022-11-10 20:10:20 +0530

    Commit 17

---
+++ keepTakingNumFromUserUntilOddEntered.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+ 
+int main() {
+    printf("Enter numbers : \n");
+    int n;
+    do
+    {
+        scanf("%d",&n);
+        printf("%d\n",n);
+        if(n%2!=0)
+        break;
+    } while (1);
+    printf("Thank You!");
+    return 0;
+}
