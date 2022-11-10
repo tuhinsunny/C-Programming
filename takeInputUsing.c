8e7938432bac4b4ab498a6c165a551db0aa13a89
Tuhin Chandra <tuhinchandra2k04@gmail.com>
2022-11-10 20:22:39 +0530

    Commit 33

---
+++ takeInputUsing.c
@@ -0,0 +1,15 @@
+//take input using %c
+#include<stdio.h>
+ 
+int main() {
+    char str[100];
+    char ch;
+    int i=0;
+    while(ch != '\n') {
+        scanf("%c" , &ch);
+        str[i]=ch;
+        i++;
+    }
+    str[i]='\0';
+    puts(str);
+}
