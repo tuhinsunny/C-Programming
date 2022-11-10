5b358478101bce9c362766848eb5d96a420e5568
Tuhin Chandra <tuhinchandra2k04@gmail.com>
2022-11-10 20:11:53 +0530

    Commit 19

---
+++ oldToNEwStr.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<string.h>
+
+int main() {
+    // char oldStr[100] = "oldStr";
+    // char newStr[] = "newStr";
+    // strcat(oldStr,newStr);
+    // // strcpy(newStr , oldStr);
+    // // puts(newStr);  
+    // puts(oldStr);
+
+    char str[] = "Banana";
+    char st[]= "Banana";
+    printf("%d",strcmp(str,st));
+    return 0;
+}
