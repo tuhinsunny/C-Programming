8cf2505f0df2a145bcaa1775be2c2c69372e23fa
Tuhin Chandra <tuhinchandra2k04@gmail.com>
2022-11-10 20:25:34 +0530

    Commit 37

---
+++ usingFunctions.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+ 
+void hello();
+void goodbye();
+int main() {
+    hello();
+    goodbye();
+    return 0;
+}
+void hello() {
+    printf("Hello\n");
+}
+void goodbye() {
+    printf("Good Bye\n");
+}
